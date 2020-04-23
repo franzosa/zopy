ccf6e7a3bcea19605246083913a1970db8bca1c9
franzosa <eric.franzosa@gmail.com>
2020-04-22 20:08:54 -0400

    untested grouping changes

--- zopy/table.py
+++ zopy/table.py
@@ -18,42 +18,11 @@ import zopy.utils as zu
 # constants
 #-------------------------------------------------------------------------------
 
-c_origin = "#"
-c_na = "#N/A"
 c_max_print = 5
 
 #-------------------------------------------------------------------------------
 # helper functions
 #-------------------------------------------------------------------------------
-
-def load_from_nested_dicts( T, dd ):
-    if T.rowheads == []:
-        T.rowheads = sorted( dd )
-    if T.colheads == []:
-        unique = set( )
-        for k, inner in dd.items( ):
-            unique.update( inner )
-        T.colheads = sorted( unique )
-    for r in T.rowheads:
-        inner = dd.get( r, {} )
-        T.data.append( [inner.get( c, T.missing ) for c in T.colheads] )
-        
-def load_from_nested_lists( T, rows ):
-    counter = 0
-    for row in rows:
-        if counter == 0:
-            T.origin = row[0] if not T.headless else c_origin
-            T.colheads = row[1:] if not T.headless else list( range( 1, len( row ) + 1 ) )
-        else:
-            T.rowheads.append( row[0] if not T.headless else counter )
-            T.data.append( row[1:] if not T.headless else row )       
-        counter += 1
-
-def load_from_file_handle( T, fh ):
-    load_from_nested_lists( T, csv.reader( fh, dialect="excel-tab" ) )
-
-def load_from_file( T, path ):
-    load_from_file_handle( T, zu.try_open( path ) )
 
 def deduplicate( items ):
     seen = set( )
@@ -78,41 +47,31 @@ def pretty_list( items ):
 #-------------------------------------------------------------------------------
 
 class Table:
+
+    #### INIT ####
     
-    def __init__( self,
-                  source=None,
-                  data=None,
-                  colheads=None,
-                  rowheads=None,
-                  origin=None,
-                  missing=None,
-                  headless=False,
-                  transposed=False,
-                  verbose=True,
-    ):
+    def __init__( self, source=None, data=None, colheads=None, rowheads=None,
+                  origin="#", missing="#N/A", headless=False, transposed=False, verbose=True ):
 
-        self.source = source
-        
-        self.data = data if data is not None else []
-        self.rowheads = rowheads if rowheads is not None else []
-        self.colheads = colheads if colheads is not None else []
-
-        self.origin = origin if origin is not None else c_origin
-        self.missing = missing if missing is not None else c_na
-        
+        # set up object attributes
+        self.source     = source
+        self.data       = data if data is not None else []
+        self.rowheads   = rowheads if rowheads is not None else []
+        self.colheads   = colheads if colheads is not None else []
+        self.origin     = origin
+        self.missing    = missing
         self.transposed = transposed
-        self.headless = headless
-        self.verbose = verbose
-
+        self.headless   = headless
+        self.verbose    = verbose
         self.sourcename = None
-        self.rowmap = None
-        self.colmap = None
+        self.rowmap     = None
+        self.colmap     = None
         
         # decide how to load table
         if source is None:
             self.sourcename = "<runtime>"
             if not all( [data, rowheads, colheads] ):
-                zu.die( "If no source provided, must provide data/rowheads/colheads" )
+                zu.die( "If no <source> then <data/rowheads/colheads> required" )
         elif isinstance( source, list ):
             self.sourcename = "<list of lists>"
             load_from_nested_lists( self, source )
@@ -130,16 +89,49 @@ class Table:
         self.remap( )
         self.report( "New table with size {}.".format( self.size( ) ) )
 
+    #### LOADERS ####
+        
+    def load_from_nested_dicts( self, dd ):
+        if self.rowheads == []:
+            self.rowheads = sorted( dd )
+        if self.colheads == []:
+            unique = set( )
+            for k, inner in dd.items( ):
+                unique.update( inner )
+                self.colheads = sorted( unique )
+        for r in self.rowheads:
+            inner = dd.get( r, {} )
+            self.data.append( [inner.get( c, self.missing ) for c in self.colheads] )
+
+    def load_from_nested_lists( self, rows ):
+        counter = 0
+        for row in rows:
+            if counter == 0:
+                self.origin = row[0] if not self.headless else self.origin
+                self.colheads = row[1:] if not self.headless else list( range( 1, len( row ) + 1 ) )
+            else:
+                self.rowheads.append( row[0] if not self.headless else counter )
+                self.data.append( row[1:] if not self.headless else row )       
+            counter += 1
+
+    def load_from_file_handle( self, fh ):
+        load_from_nested_lists( self, csv.reader( fh, csv.excel_tab ) )
+
+    def load_from_file( self, path ):
+        load_from_file_handle( self, zu.try_open( path ) )
+
+    #### TEST AND MAP ####
+        
     def remap( self ):
 
         # integrity checks
         if len( self.data ) > 0:
             if len( set( [len( row ) for row in self.data] ) ) != 1:
-                zu.die( "Table has inconsistent row lengths." )
+                zu.die( "Table has inconsistent row lengths" )
             if len( self.colheads ) != len( self.data[0] ):
-                zu.die( "Table colheads do not align to data." )
+                zu.die( "Colheads do not align to data" )
             if len( self.rowheads ) != len( self.data ):
-                zu.die( "Table rowheads do not align to data." )
+                zu.die( "Rowheads do not align to data" )
 
         # auto-replace duplicate field names
         deduplicate( self.rowheads )
@@ -148,7 +140,7 @@ class Table:
         # build fast maps
         self.rowmap = {k:i for i, k in enumerate( self.rowheads )}
         self.colmap = {k:i for i, k in enumerate( self.colheads )}
-
+    
     #### UTILITY METHODS ####
     
     def rowdex( self, index ):
@@ -193,10 +185,10 @@ class Table:
         fh = sys.stdout
         if path is not None:
             fh = zu.try_open( path, "w" )
-        W = csv.writer( fh, dialect="excel-tab" )
+        W = csv.writer( fh, csv.excel_tab )
         W.writerow( [self.origin] + self.colheads )
-        for i, row in enumerate( self.data ):
-            W.writerow( [self.rowheads[i]] + row )
+        for r, row in zip( self.rowheads, self.data ):
+            W.writerow( [r] + row )
         if path is not None:
             fh.close( )
 
@@ -231,7 +223,8 @@ class Table:
         return {self.rowheads[k]:col[k] for k in range( len( self.rowheads ) )}
 
     def get( self, r, c ):
-        r, c = self.rowdex( r ), self.coldex( c )
+        r = self.rowdex( r )
+        c = self.coldex( c )
         return self.data[r][c]
 
     #### APPLY FUNCTIONS ####
@@ -243,7 +236,7 @@ class Table:
         self.transposed = not self.transposed
         self.remap( )
         self.report( "Transposed the table." )
-        
+    
     def promote( self, index ):
         """ pick a row to become the new row[0]; i.e. make it the colhead row """
         self.colheads = self.data.pop( self.rowdex( index ) )
@@ -291,59 +284,59 @@ class Table:
         """ iterate over cols; yields colhead, col """                         
         for c in self.colheads:
             yield c, self.col( c )
-
+    
     def iter_entries( self ):
         """ iterate over entries; yields ( r )owhead, ( c )olhead, entry( r, c ) """
         for r in self.rowheads:
             for c in self.colheads:
                 yield r, c, self.get( r, c )
-
-    #### FILTER OPERATIONS ####               
-
-    def filter( self,
-                function,
-                t=None, transposed=False,
-                i=None, invert=False,
-                n=None, new=False,
-    ):
+    
+    #### FILTER OPERATIONS ####
+    
+    def filter( self, function,
+                t=False, transposed=False,
+                i=False, invert=False,
+                n=False, new=False, ):
 
         """ apply a function to the rows of table (via headers) and rebuild or return true evals """
-
+        
         # process shortcuts 
-        transposed = transposed if t is None else t
-        invert = invert if i is None else i
-        new = new if n is None else n
-
+        transposed = any( [t, transposed] )
+        invert = any( [i, invert] )
+        new = any( [n, new] )
+        
         # flip?
-        if transposed: self.transpose( )
-
+        if transposed:
+            self.transpose( )
+        
         # apply test to rows
         mask = [function( r ) for r in self.rowheads]
         mask = mask if not invert else [not k for k in mask]
-
-        # (1) rebuild table
+        
+        # option 1: in place filtering of rows
+        ret = None
         if not new:
             self.data = [row for test, row in zip( mask, self.data ) if test]
             self.rowheads = [r for test, r in zip( mask, self.rowheads ) if test]
-            if transposed: self.transpose( )
+            if transposed:
+                self.transpose( )
             self.remap( )
             self.report( "new size is", self.size( ) )
-            return None
-        # (2) return rows that passed as new table
+        # option 2: return new table with filtered rows
         else:
-            new_table = Table(
-                data = [row[:] for test, row in zip( mask, self.data ) if test],
-                rowheads = [r for test, r in zip( mask, self.rowheads ) if test],
-                colheads = self.colheads[:],
-                origin = self.origin,
-                missing = self.missing,
+            ret = Table(
+                data       = [row[:] for test, row in zip( mask, self.data ) if test],
+                rowheads   = [r for test, r in zip( mask, self.rowheads ) if test],
+                colheads   = self.colheads[:],
+                origin     = self.origin,
+                missing    = self.missing,
                 transposed = self.transposed,
-                verbose = self.verbose,
+                verbose    = self.verbose,
             )
             if transposed:
                 self.transpose( )
-                new_table.transpose( )
-            return new_table
+                ret.transpose( )
+        return ret
 
     #### METHODS THAT USE FILTER ####
 
@@ -410,14 +403,81 @@ class Table:
         self.report( "applying nontrivial", "requiring at least", mincount, "values exceeding", minlevel, kwargs )
         return self.filter( function, **kwargs )
 
-    # **** updated to here ****
-    
-    # ---------------------------------------------------------------
-    # groupby
-    # ---------------------------------------------------------------
+    #### REGROUPING METHODS ####
 
-    def groupby( self, funcGrouper, funcSummarizer ):
-        """ user grouper function on rowheads to cluster rows, then use summarizer function to combine values """ 
+    def stratify( self, grouper, t=False, transposed=False ):
+        """ stratify table on function of rowheads; returns dictionary of tables """
+        transposed = any( [t, transposed] )
+        if transposed:
+            self.transpose( )
+        # index which original rows below to which strata
+        tables = {}
+        for r in self.rowheads:
+            key = grouper( r )
+            tables.setdefault( key, [] ).append( r )
+        # make a new table out of stratified rows
+        for key, rowheads in tables.items( ):
+            tables[key] = Table(
+                colheads   = self.colheads[:],
+                rowheads   = rowheads,
+                data       = [self.row( r ) for r in rowheads],
+                origin     = self.origin,
+                missing    = self.missing,
+                transposed = self.transposed,
+                verbose    = self.verbose,
+            )
+            if transposed:
+                tables[key].transpose( )
+        if transposed:
+            self.transpose( )
+        self.report( "stratified into", len( tables ), "new tables with keys like", key )
+        return tables
+
+    def groupby( self, grouper, summarizer, t=False, transposed=False, n=False, new=False ):
+        """ group rows on a function of the rowheads; collapse grouped values using summarizer """        
+        transposed = any( [t, transposed] )
+        new = any( [n, new] )
+        if transposed:
+            self.transpose( )
+        # stack row values according to grouper function
+        data = {}
+        for r in self.rowheads:
+            key = grouper( r )
+            if key not in data:
+                data[key] = [[] for c in self.colheads]
+            for value, group in zip( self.row( r ), data[key] ):
+                group.append( value )
+        new_rowheads = sorted( data )
+        # apply summarizer to stacks to make new rows
+        new_data = [[summarizer( group ) for group in data[key]] for key in rowheads]
+        # **** ~copied from <filter> ****
+        ret = None
+        if not new:
+            self.rowheads = new_rowheads
+            self.data = new_data
+            if self.transposed:
+                self.transpose( )
+            self.remap( )
+            self.report( "after groupby rowheads like <%s>" % ( self.rowheads[0] ), "; new size is", self.size( ) )
+        else:
+            ret = Table(
+                data       = new_data,
+                rowheads   = new_rowheads,
+                colheads   = self.colheads[:],
+                origin     = self.origin,
+                missing    = self.missing,
+                transposed = self.transposed,
+                verbose    = self.verbose,
+            )
+            if transposed:
+                self.transpose( )
+                ret.transpose( )
+        return ret
+
+    # **** updated to here ****
+
+    """
+    def groupby( self, grouper, summarizer ):
         groups = {} # map new rowheads to 1+ old rowheads
         for rowhead in self.rowheads:
             groupdict = groups.setdefault( funcGrouper( rowhead ), {} )
@@ -430,13 +490,11 @@ class Table:
         self.data = data2
         self.remap()
         self.report( "applied groupby:", "rowheads now like <%s>" % ( self.rowheads[0] ), "; new size is", self.size() )
+    """
 
-    # ---------------------------------------------------------------
-    # stratification methods
-    # ---------------------------------------------------------------
-
+    """"
     def stratify( self, focus ):
-        """ stratifies table on focal row value; returns dictionary of tables """
+        # stratifies table on focal row value; returns dictionary of tables
         self.transpose()
         dictDataArrays = {}
         for rowhead, row in self.iter_rows( ):
@@ -450,9 +508,11 @@ class Table:
         self.transpose()
         self.report( "stratified on", focus, "into", len( dictTables ), "new tables" )
         return dictTables
+    """
 
+    """
     def funcify( self, func ):
-        """ stratifies table on function return value """
+        # stratifies table on function return value
         self.transpose( )
         dictDataArrays = {}
         for rowhead, row in self.iter_rows( ):
@@ -467,9 +527,11 @@ class Table:
         self.transpose( )
         self.report( "funcified into", len( dictTables ), "new tables" )
         return dictTables
+    """
 
+    """
     def groupify( self, funcGrouper ):
-        """ stratifies table by return value of function on rowheads; returns dictionary of tables """
+        # stratifies table by return value of function on rowheads; returns dictionary of tables
         dictDataArrays = {}
         for rowhead, row in self.iter_rows():
             group = funcGrouper( rowhead )
@@ -480,6 +542,7 @@ class Table:
             dictTables[group] = table( aaData, verbose=self.isverbose )
         self.report( "groupified on f( rowhead ) into", len( dictTables ), "new tables" )
         return dictTables
+    """
 
     # ---------------------------------------------------------------
     # conversion methods
@@ -518,7 +581,7 @@ class Table:
             self.report( "augmentee contains", len( setRowheadOverlap ), "duplicate rowheads (skip)" )
         # note: "colhead in dictColmap" much faster than "colhead in aColheads"
         self.data = self.data + [
-            [rowhead2] + [table2.get( rowhead2, colhead ) if colhead in table2.colmap else c_na for colhead in self.colheads] 
+            [rowhead2] + [table2.get( rowhead2, colhead ) if colhead in table2.colmap else self.missing for colhead in self.colheads] 
             for rowhead2 in table2.rowheads if rowhead2 not in self.rowmap
             ]
         self.remap()
@@ -542,7 +605,7 @@ class Table:
                 self.data[i] += [row[j] for j in aIndex]
             # a self-specific row, add all NA entries
             else:
-                self.data[i] += [c_na for j in aIndex]
+                self.data[i] += [self.missing for j in aIndex]
         self.remap()
         self.report( "extended with cols from", table2.source, "new size is", self.size() )
 
@@ -555,13 +618,13 @@ class Table:
     # methods for filling "empty" cells
     # ---------------------------------------------------------------
 
-    def blank2na( self, value=c_na ):
+    def blank2na( self ):
         """ Anything that doesn't match a non-space char is filled with na """
-        self.apply_entries( lambda k: k if re.search( "[^\s]", str( k ) ) else value )
+        self.apply_entries( lambda k: k if re.search( "[^\s]", str( k ) ) else self.missing )
         
     def na2zero( self, value=0 ):
         """ Convert NAs to zero values """
-        self.apply_entries( lambda k: value if k == c_na else k )
+        self.apply_entries( lambda k: value if k == self.missing else k )
 
     # ---------------------------------------------------------------
     # methods for working with metadata
@@ -633,6 +696,6 @@ class Table:
         # flip back
         self.transpose()
 
-    # ---------------------------------------------------------------
-    # end of the table object
-    # ---------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# end of table object and file
+#-------------------------------------------------------------------------------
